09bc5334e950530003a585958dcfd612c4ef6510
Loser <58706003+Perdente@users.noreply.github.com>
2022-02-13 20:14:28 +0600

    Create (Jun 15, 2021)2.cpp

---
+++ Enosis Solutions/(Jun 15, 2021)2.cpp
@@ -0,0 +1,4 @@
+/*
+The interview gave a list of nodes from a collection of trees where each node has an id and a name (string) 
+and another id which determines its parent(-1 in case the node is a root). The trees (forest) had to be constructed from the given information.
+*/ 
