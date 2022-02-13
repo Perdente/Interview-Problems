d87223c343470041c0f321081590c7c65038c6e4
Loser <58706003+Perdente@users.noreply.github.com>
2022-02-13 19:45:17 +0600

    Create (Jan 7, 2022)1.cpp

---
+++ Enosis Solutions/(Jan 7, 2022)1.cpp
@@ -0,0 +1,41 @@
+/*
+Longest Common subsequence of string
+*/
+
+const int N = 3e3 + 5;
+int dp[N][N];
+string a, b;
+int lcs_length(int i, int j) {
+    int n = a.size(), m = b.size();
+    if (i == n or j == m) return 0;
+    if (dp[i][j] != -1) return dp[i][j];
+    if (a[i] == b[j]) return lcs_length(i + 1, j + 1) + 1;
+    int x = 0, y = 0;
+    x = lcs_length(i + 1, j);
+    y = lcs_length(i, j + 1);
+    return dp[i][j] = max(x, y);
+}
+
+void print_lcs(int i, int j) {
+    int n = a.size(), m = b.size();
+    if (i == n or j == m) return;
+    if (a[i] == b[j]) {
+        cout << a[i];
+        print_lcs(i + 1, j + 1);
+        return;
+    }
+    int x = 0, y = 0;
+    x = lcs_length(i + 1, j);
+    y = lcs_length(i, j + 1);
+    if (x >= y) {
+        print_lcs(i + 1, j);
+    }else {
+        print_lcs(i, j + 1);
+    }
+}
+
+void malena() {
+    cin >> a >> b;
+    memset(dp, -1, sizeof dp);
+    print_lcs(0, 0);
+}
