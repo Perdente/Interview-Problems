981c8eccb8af9d8d37333598402cc811e1508da8
Loser <58706003+Perdente@users.noreply.github.com>
2022-03-01 21:20:25 +0600

    Update (Jul 18, 2021)1.cpp

--- Enosis Solutions/(Jul 18, 2021)1.cpp
+++ Enosis Solutions/(Jul 18, 2021)1.cpp
@@ -1,1 +1,27 @@
 /*Given a list of tree node. Generate the full tree using parent and child ID.*/
+/*
+LinkedIn Interview question:
+Given a list of child->parent relationships, build a binary tree out of it. All the element Ids inside the tree are unique.
+
+Example:
+
+Given the following relationships:
+
+Child Parent IsLeft
+15 20 true
+19 80 true
+17 20 false
+16 80 false
+80 50 false
+50 null false
+20 50 true
+
+
+You should return the following tree:
+    50
+  /   \
+ 20    80
+/ \    / \
+15 17 19 16
+
+*/
