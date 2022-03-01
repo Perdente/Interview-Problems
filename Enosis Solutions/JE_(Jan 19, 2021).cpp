7404e5ff580d08074676bccebcc49f70595c7ba0
Loser <58706003+Perdente@users.noreply.github.com>
2022-03-01 12:25:15 +0600

    Create JE_(Jan 19, 2021).cpp

---
+++ Enosis Solutions/JE_(Jan 19, 2021).cpp
@@ -0,0 +1,2 @@
+//  Given a long string and a line width, modify the string to put new line so that a line has at most line width amount of character 
+//  and no line break is given in the middle of a word. I was not allowed to use split function.
