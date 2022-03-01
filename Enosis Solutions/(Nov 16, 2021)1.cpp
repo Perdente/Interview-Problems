89cc71024ebec0a1a77677ca56ea8f30fed80772
Loser <58706003+Perdente@users.noreply.github.com>
2022-03-01 12:02:19 +0600

    Update (Nov 16, 2021)1.cpp

--- Enosis Solutions/(Nov 16, 2021)1.cpp
+++ Enosis Solutions/(Nov 16, 2021)1.cpp
@@ -1,3 +1,15 @@
 /*
 Find a string is anagram or not?
+An anagram of a string is another string that contains the same characters, only the order of characters can be different.
+For example, “abcd” and “dabc” are an anagram of each other.
 */
+int arr[26];
+void malena() {
+    string s, t; cin >> s >> t;
+    for (auto ch: s) arr[ch - 'a']++;
+    for (auto ch: t) arr[ch - 'a']--;
+    for (int i = 0; i < 26; ++i) {
+        if (arr[i]) {cout << "No" << endl; return;}
+    }
+    cout << "Yes\n";
+}
