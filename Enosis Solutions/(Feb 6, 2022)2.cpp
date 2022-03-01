27048772f613921cc3853622a4133dc8fe184637
Loser <58706003+Perdente@users.noreply.github.com>
2022-03-01 19:53:44 +0600

    Update (Feb 6, 2022)2.cpp

--- Enosis Solutions/(Feb 6, 2022)2.cpp
+++ Enosis Solutions/(Feb 6, 2022)2.cpp
@@ -12,28 +12,27 @@ Output this pattern as center-aligned
 */
 
 // Printing Pascal's Triangle Left Aligned - 
-int Pascal (int row, int column) {
-  if (column == 0)
-    return 1;
-  if (row == column)
-    return 1;
-  return Pascal(row - 1, column - 1) + Pascal(row - 1, column);
+int dp[1000][1000];
+int pascal_triangle(int row, int col) {
+    if (col == 0 or row == col) return 1;
+    if (dp[row][col] != -1) return dp[row][col];
+    return dp[row][col] = pascal_triangle(row - 1, col - 1) + pascal_triangle(row - 1, col);
 }
 
 void malena() {
-    int n; cin >> n;
-    cout << "Input is - " << n << '\n';
-
-    cout << "Output ::: \n";
-    for (int i = 0; i <= n; ++i) {
+    int numRows; cin >> numRows;
+    vector<vector<int>> ans(numRows);
+    memset(dp, -1, sizeof dp);
+    for (int i = 0; i < numRows; ++i) {
         for (int j = 0; j <= i; ++j) {
-            cout << Pascal(i, j) << " ";
+            cout << pascal_triangle(i, j) << " ";
         }
         cout << '\n';
     }
 }
+
 /*
-Input is - 6
+Input is - 7
 Output ::: 
 1 
 1 1 
@@ -48,60 +47,34 @@ Output :::
 
 // Printing Pascal's Triangle Center Aligned
 
-// Recursive code to calculate Pascal's Triangle
-int Pascal (int row, int column) {
-  if (column == 0) return 1;
-  if (row == column) return 1;
-  return Pascal(row - 1, column - 1) + Pascal(row - 1, column);
-}
-// Converts a row from Pascal's Triangle from integer to a string
-int rowLength(int row, int column) {
-    int current_row;
-    string current_row_char = "";
-    stringstream out;
-    for (int current_row = row; current_row <= row; ++current_row) {
-        for (int c = 0; c <= row; ++c) {
-            out << " " << Pascal(row, column) << " ";
-        }
-        current_row_char += out.str();
-    }
-    return current_row_char.length();
+int dp[1000][1000];
+int pascal_triangle(int row, int col) {
+    if (col == 0 or row == col) return 1;
+    if (dp[row][col] != -1) return dp[row][col];
+    return dp[row][col] = pascal_triangle(row - 1, col - 1) + pascal_triangle(row - 1, col);
 }
 
 void malena() {
-    int tot_row; cin >> tot_row;
-    cout << "Input is - " << tot_row << '\n';
-    cout << "Output - \n";
-    // determination of how long the bottom row is
-    int bottom_row;
-    string bottom_row_char;
-    stringstream out;
-    for (int r = tot_row; r <= tot_row; ++r) {
-        for (int c = 0; c <= r; ++c) {
-            out << " " << Pascal(r, c) << " ";
-        }
-        bottom_row_char += out.str();
-    }
-    // Printing actual Pascal's Triangle
-    for (int row = 0; row <= tot_row; ++row) {
-        cout.width((bottom_row_char.length() - rowLength(row, 0)) / 2);
-        for (int col = 0; col <= row; ++col) {
-            cout << " " << Pascal(row, col) << " ";
+    int numRows; cin >> numRows;
+    vector<vector<int>> ans(numRows);
+    memset(dp, -1, sizeof dp);
+    for (int i = 0; i < numRows; ++i) {
+        cout << string(numRows - i - 1, ' '); // printing spaces for each row
+        for (int j = 0; j <= i; ++j) {
+            cout << pascal_triangle(i, j) << " ";
         }
         cout << '\n';
     }
-    cout << '\n';
 }
-
 /*
 Input is - 5
 Output - 
-         1 
-        1  1 
-      1  2  1 
-     1  3  3  1 
-   1  4  6  4  1 
- 1  5  10  10  5  1 
+         1          space - 5
+        1  1        space - 4
+      1  2  1       space - 3
+     1  3  3  1     space - 2
+   1  4  6  4  1    space - 1
+ 1  5  10  10  5  1 space - 0
 
 */
 
