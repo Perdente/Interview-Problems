bfdf5e03d4b426cbe971dc1262db07e4f729baeb
Partha P. Mazumder <58706003+Perdente@users.noreply.github.com>
2022-03-24 11:59:49 +0600

    Update stringToLower.cs

--- C# Programs(might come handy)/stringToLower.cs
+++ C# Programs(might come handy)/stringToLower.cs
@@ -1,1 +1,20 @@
 
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    string test = "LoserShit";
+    test = test.ToLower();
+    for (int i = 0; i < test.Length; ++i) {
+        Console.Write(test[i] + " ");
+    }
+  }
+  
+}
