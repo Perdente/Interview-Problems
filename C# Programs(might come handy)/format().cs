69adb000fc51ccb6d89a1465331d12efc6ef5085
Partha P. Mazumder <58706003+Perdente@users.noreply.github.com>
2022-03-24 12:30:09 +0600

    Update format().cs

--- C# Programs(might come handy)/format().cs
+++ C# Programs(might come handy)/format().cs
@@ -1,1 +1,20 @@
 
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    string a = "Partha", b = "Pratim", c = "Mazumder";
+    string result = string.Format("\"{0}\", is my first initial, \"{1}\" is my second initial, \"{2}\" is my final initial", a, b, c);
+    Console.WriteLine(result);
+  }
+  
+}
+
+// "Partha", is my first initial, "Pratim" is my second initial, "Mazumder" is my final initial
