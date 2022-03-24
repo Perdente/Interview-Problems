442ea63c724eeda4cf644ff5c24d0d793ebc9e11
Partha P. Mazumder <58706003+Perdente@users.noreply.github.com>
2022-03-24 22:41:24 +0600

    Update (Jan 7, 2022)1.cpp

--- Enosis Solutions/(Jan 7, 2022)1.cpp
+++ Enosis Solutions/(Jan 7, 2022)1.cpp
@@ -39,3 +39,36 @@ void malena() {
     memset(dp, -1, sizeof dp);
     print_lcs(0, 0);
 }
+
+
+
+
+
+
+
+
+
+/*
+using System;
+class HelloWorld {
+  static void Main() {
+   string s = "LoserPartha";
+   string t = "Partha";
+   char[] a = s.ToCharArray();
+   char[] b = t.ToCharArray();
+   int ans = lcs_length(0, 0, a, b);
+   Console.WriteLine(ans);
+  }
+  public static int lcs_length(int i, int j, char[] a, char[] b) {
+    int n = a.Length;
+    int m = b.Length;
+    if (i == n || j == m) return 0;
+    if (a[i] == b[j]) return lcs_length(i + 1, j + 1, a, b) + 1;
+    int x = 0;
+    int y = 0;
+    x = lcs_length(i + 1, j, a, b);
+    y = lcs_length(i, j + 1, a, b);
+    return Math.Max(x, y);
+  }
+}
+*/
