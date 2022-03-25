6002781418ee9a895942f893de411c1dcb79bb09
Partha P. Mazumder <58706003+Perdente@users.noreply.github.com>
2022-03-25 20:25:59 +0600

    Update (Nov 16, 2021)1.cpp

--- Enosis Solutions/(Nov 16, 2021)1.cpp
+++ Enosis Solutions/(Nov 16, 2021)1.cpp
@@ -13,3 +13,44 @@ void malena() {
     }
     cout << "Yes\n";
 }
+
+
+
+/*
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    int[] arr = new int[26];
+    string s = "maddam";
+    string t = "mdadam";
+    Console.WriteLine($"First string is = {s}");
+    Console.WriteLine($"Second string is = {t}");
+    char[] sc = s.ToCharArray();
+    char[] tc = t.ToCharArray();
+    
+    foreach (var ch in sc) {
+        if (arr[ch - 'a'] == 0) arr[ch - 'a'] = 1;
+        else arr[ch - 'a']++;
+    }
+    
+    foreach (var ch in tc) {
+        if (arr[ch - 'a'] >= 1) arr[ch - 'a']--;
+    }
+    bool flag = true;
+    foreach (var st in arr) {
+        if (st >= 1) flag = false;
+    }
+    if (flag) Console.WriteLine("Anagram");
+    else Console.WriteLine("Anagram");
+  }
+}
+
+*/
